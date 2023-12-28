ef9f8cd21524151b61a8bbc15e63fa50520f0d71
babucarr32 <baboucarry@gmai.com>
2023-12-28 14:30:03 +0000

    Initialozed

---
+++ vite.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import dts from "vite-plugin-dts";
+import path from "path";
+
+import eslint from "vite-plugin-eslint";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "index.ts"),
+      name: "ViteButton",
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  plugins: [react(), dts(), eslint()],
+});

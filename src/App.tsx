a555c2ee6e89675ea3bb9c6cbcba023c4d2cd834
babucarr32 <baboucarry@gmai.com>
2023-12-29 07:07:37 +0000

    Refactor code

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ImageUpload from '@/components/ImageUpload';
+import useSelectFile from './hooks/useSelectFile';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const {error, imgsToSave, selectedImages} = useSelectFile()
+  console.log({imgsToSave, selectedImages})
+  console.log(imgsToSave)
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>{error.message}</h1>
+      <ImageUpload quality={1}
+        Content={<p>UPLOAD</p>}
+        deleteIcon={<p>DEL</p>}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;

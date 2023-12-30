aad64f978e0b03ab8a1e3ac0d22ee586aae87456
babucarr32 <baboucarry@gmai.com>
2023-12-30 21:11:19 +0000

    Add ImageInput component

---
+++ src/components/ImageInput.tsx
@@ -0,0 +1,42 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useEffect } from 'react';
+
+import useSelectFile from '../hooks/useSelectFile';
+import { FileUploadType } from "../../types";
+import { jotaiFileLimit, jotaiFileSizeLimit } from '@/JotaiAtoms';
+import { useAtom } from 'jotai';
+
+const ImageInput: React.FC<FileUploadType> = ({ quality, content, limit, fileSizeLimit }) => {
+  const {handleSelectFile} = useSelectFile();
+  const [fileLimit, setFileLimit] = useAtom(jotaiFileLimit)
+  const [maxFileSize, setMaxFileSize] = useAtom(jotaiFileSizeLimit)
+
+  useEffect(() => {
+    const handleSetFileLimit = () => limit && setFileLimit(limit)
+    handleSetFileLimit()
+  }, [])
+
+  useEffect(() => {
+    const handleSetFileSizeLimit = () => fileSizeLimit && setMaxFileSize(fileSizeLimit)
+    handleSetFileSizeLimit()
+  }, [])
+
+  return (
+    <>
+      <input
+        hidden
+        multiple
+        type='file'
+        id='fileUpload'
+        className='w-52'
+        name='fileUpload'
+        onClick={(e: any) => e.target.value = ""}
+        onChange={(e) => handleSelectFile(e, quality, fileLimit, maxFileSize)}
+      />
+      <label htmlFor='fileUpload'>{content || "Upload"}</label>
+    </>
+  );
+};
+
+export default ImageInput;

3dde1a21d4930b518630c5c9537f292a137d2713
babucarr32 <baboucarry@gmai.com>
2023-12-30 21:10:56 +0000

    Add DragAndDrop component

---
+++ src/components/DragAndDrop.tsx
@@ -0,0 +1,52 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { createContext, useEffect } from "react";
+import { useAtom } from "jotai";
+
+import useSelectFile from "@/hooks/useSelectFile";
+import { FileUploadType } from "../../types";
+import { jotaiFileLimit, jotaiFileSizeLimit } from "@/JotaiAtoms";
+
+export const UploadContext = createContext(null);
+
+const DragAndDrop: React.FC<FileUploadType> = ({ quality, content, className, limit, fileSizeLimit }) => {
+  const { handleDropFile} = useSelectFile()
+  const [fileLimit, setFileLimit] = useAtom(jotaiFileLimit)
+  const [, setMaxFileSize] = useAtom(jotaiFileSizeLimit)
+
+
+  useEffect(() => {
+    const handleSetFileLimit = () => limit && setFileLimit(limit)
+    handleSetFileLimit()
+  }, [])
+
+  useEffect(() => {
+    const handleSetFileSizeLimit = () => fileSizeLimit && setMaxFileSize(fileSizeLimit)
+    handleSetFileSizeLimit()
+  }, [])
+
+  return (
+    <>
+      <div
+        className={className}
+        id="drop_zone"
+        onDrop={(e) => (
+          handleDropFile(e, quality, fileLimit),
+          e.preventDefault()
+        )}
+        onDragOver={(e) => (
+          e.preventDefault()
+        )}
+        onDragExit={() => {}}
+      >{
+          content ||
+          <div>
+            <p>Drag and Drop image(s) here.</p>
+          </div>
+        }
+      </div>
+    </>
+  );
+}
+
+export default DragAndDrop;

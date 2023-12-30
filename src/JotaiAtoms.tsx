7b39f585dc6d0bbf22f5f4a5933cc27c4414d44f
babucarr32 <baboucarry@gmai.com>
2023-12-30 21:10:04 +0000

    Add new atoms

--- src/JotaiAtoms.tsx
+++ src/JotaiAtoms.tsx
@@ -1,5 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { atom } from 'jotai';
 
-export const uploadedPostImg = atom<string[]>([]);
+import { DetailsType, ErrorType } from 'types';
+
 export const imagesToSave = atom<Blob[]>([]);
+export const jotaiImages = atom<string[]>([]);
+export const jotaiLimitCount = atom<number>(1);
+export const jotaiFileLimit = atom<number>(100);
+export const jotaiFileSizeLimit = atom<string>("");
+export const jotaiError = atom<ErrorType>({message: ""})
+export const jotaiDetails = atom<DetailsType[]>([{type: "", size: 0}])

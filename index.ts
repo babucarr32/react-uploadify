4b6c5987eebfbd4ddeaa0aa8ae24671bfd76bbc0
babucarr32 <baboucarry@gmai.com>
2024-01-01 10:39:52 +0000

    export components

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-export {};
+import Images from "@/components/Images";
+import useSelectFile from "@/hooks/useSelectFile";
+import ImageUpload from "@/components/ImageUpload";
+import DragAndDrop from "@/components/DragAndDrop";
+import useImageUpload from "@/hooks/useImageUpload";
+
+export {
+  Images,
+  DragAndDrop,
+  ImageUpload,
+  useSelectFile,
+  useImageUpload
+};

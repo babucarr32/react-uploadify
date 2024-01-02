bf907d3cd85da768f220e86dccce8f31a2e534a5
babucarr32 <baboucarry@gmai.com>
2024-01-02 15:10:13 +0000

    export reset function

--- src/hooks/useImageUpload.tsx
+++ src/hooks/useImageUpload.tsx
@@ -2,8 +2,9 @@ import { UseImageUploadType } from "../types";
 import useSelectFile from "./useSelectFile";
 
 const useImageUpload = (): UseImageUploadType => {
-  const { blobImages, details, error, images, isDraggedOver } = useSelectFile();
-  return { blobImages, details, error, images, isDraggedOver };
+  const { blobImages, details, error, images, isDraggedOver, reset } =
+    useSelectFile();
+  return { blobImages, details, error, images, isDraggedOver, reset };
 };
 
 export default useImageUpload;
